8bca0080e7e54427e1e9d364fd478a97a06a0125
PyeongGangKim <21500185@handong.edu>
2022-10-17 22:28:51 +0900

    feat: start와 startGame 분리

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,19 +1,41 @@
-import kotlin.properties.Delegates
+import java.util.Scanner
+
+
 
 class Game(
     private val computer: Computer,
     private val user: User
 ) {
     private var isDone : Boolean = false
+    private var isContinue : Boolean = true
     private var answer : List<Int> = emptyList()
     private var userAnswer: List<Int> = emptyList()
+    var scanner: Scanner = Scanner(System.`in`)
 
     fun start() {
-       answer = computer.generateNum()
-       do {
-           // user한테 입력받기
-           user.getUserAnswer()
-           // user 입력과 answer과 비교 후 힌트 or game 끝내기
-       } while (isDone)
+        while(isContinue) {
+            startGame()
+            println(GAME_MENT)
+            val continueGameNumber = scanner.nextInt()
+
+        }
+    }
+
+    private fun startGame() {
+        answer = computer.generateNum()
+        do {
+            // user한테 입력받기
+            userAnswer = user.getUserAnswer()
+            // user 입력과 answer과 비교 후 힌트 or game 끝내기
+
+        } while (isDone)
+    }
+
+    fun checkAnswer() {
+
+    }
+
+    companion object {
+        private const val GAME_MENT = "새로운 게임 시작(1), 게임 종료(2)"
     }
 }
---
+++ src/main/kotlin/Hint.kt
@@ -0,0 +1,7 @@
+class Hint(
+    var strike: Int = 0,
+    var ball: Int = 0
+) {
+
+    fun
+}
